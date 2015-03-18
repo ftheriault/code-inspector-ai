6c27b0c5d651e84c976bf75f6d19386c1c54f13c
Clément <clement.contini.1@ens.etsmtl.ca>
2015-03-18 15:23:07 -0400

    Implementation of line length rule

--- client/js/logic/LineLengthInspector.js
+++ client/js/logic/LineLengthInspector.js
@@ -7,7 +7,7 @@
 */
 function LineLengthInspector() {
 	BaseInspector.call(this, 
-				"",
+				"Clément Contini",
 				"Checks that a line must not exceed 80 characters long",
 				["Java", "Ruby"]);
 }
@@ -16,8 +16,19 @@ LineLengthInspector.prototype = new BaseInspector();
 LineLengthInspector.prototype.constructor = LineLengthInspector;
 
 LineLengthInspector.prototype.analyze = function(event) {
-	// TODO
-	// Check each line from code file and verify its length
+	var lines = event.content.split('\n');
+	var tooLong = [];
 
-	// Use this.notify(event.fileName, "Lorem ...") when a line exceed 80 characters.
+	for(var i = 0; i < lines.length; i++){
+        if (lines[i].length > 80) {
+            tooLong.push(i+1);
+        }
+    }
+    if (tooLong.length > 0) {
+        var message = "<strong>Line " + tooLong[0] + "</strong> exceeds 80 characters.";
+        if (tooLong.length > 1) {
+            message = "<strong>Lines " + tooLong.join() + "</strong> exceed 80 characters.";
+        }
+	    this.notify(event.fileName, message)
+    }
 };
