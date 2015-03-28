99df78c14fe0fcd4301f8ca657c3a509ac3b700d
Ilafaf <fafaliel@justitia>
2015-03-27 22:45:11 -0400

    Blank lines can contain spaces.

--- client/js/logic/IndentationInspector.js
+++ client/js/logic/IndentationInspector.js
@@ -22,17 +22,19 @@ IndentationInspector.prototype.analyze = function(event) {
 	var message = "";
     // '{' and '}' in comments are also taken into account 
 	for(var i = 0; i < lines.length; i++){
-        if (lines[i].indexOf("}") != -1) stack--;
-		if (lines[i].indexOf("{") != -1 && lines[i].indexOf("}") != -1) message = "Code in line <strong>" + parseInt(i+1) + "</strong> is wrongly delimited in file <strong>" + event.fileName + "</strong>.";
-		var j = 0;
-        while( j < lines[i].length - 1 && j < stack) {
-			if (lines[i].charAt(j) != '\t') {
-				message = "<strong>Line " + parseInt(i+1) + "</strong> is wrongly indented in file <strong>" + event.fileName + "</strong>.";
-			}
-            j++;
-		}
-        if (lines[i].charAt(j) == '\t') message = "<strong>Line " + parseInt(i+1) + "</strong> is wrongly indented in file <strong>" + event.fileName + "</strong>.";
-        if (lines[i].indexOf("{") != -1) stack++;
+        if (lines[i].trim() != ""){
+            if (lines[i].indexOf("}") != -1) stack--;
+            if (lines[i].indexOf("{") != -1 && lines[i].indexOf("}") != -1) message = "Code in line <strong>" + parseInt(i+1) + "</strong> is wrongly delimited in file <strong>" + event.fileName + "</strong>.";
+            var j = 0;
+            while( j < lines[i].length - 1 && j < stack) {
+                if (lines[i].charAt(j) != '\t') {
+                    message = "<strong>Line " + parseInt(i+1) + "</strong> is wrongly indented in file <strong>" + event.fileName + "</strong>.";
+                }
+                j++;
+            }
+            if (lines[i].charAt(j) == '\t') message = "<strong>Line " + parseInt(i+1) + "</strong> is wrongly indented in file <strong>" + event.fileName + "</strong>.";
+            if (lines[i].indexOf("{") != -1) stack++;
+        }
 	}
 	if (stack != 0){
 		message = "Indentation problem with file <strong>" + event.fileName + "</strong>.";
